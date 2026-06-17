d5f5d9b1a723693a4501f9c7d00154b73489d407
Anikesh soni <anikesh639414@gmail.com>
2026-06-17 23:16:28 +0530

    initial commit

--- Basics/operators.cpp
+++ Basics/operators.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main (){
-    
+    float a = 100;
+    float b = 21;
+
+    // cout<< a++ << endl;
+    // cout << ++a << endl;
+    cout << "addition :"<< a + b << endl;
+    cout << "subtraction :"<< a-b << endl;
+    cout << "product :" << a*b << endl;
+    cout << "division :"<< a/b << endl;
+    cout << "remainder :"<< fmod(a,b) << endl;
+
+    return 0;
 }
